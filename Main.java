1a46c22fe6c2452a2e4e49454792a6f086003d79
Kanishka Salgude <kanishkasalgude5@gmail.com>
2026-01-14 11:31:38 +0530

    sample operations

--- Main.java
+++ Main.java
@@ -1,51 +1,118 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        float a;
+        float b;
+        float c;
+        Scanner scanner = new Scanner(System.in);
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("OPERATIONS\n");
-        int a = 2;
-        int b = 54;
-        int choice;
-        Scanner scanner = new Scanner(System.in);
+        System.out.printf("SWAP 2 NUMBERS\nEnter first number :");
+        a = scanner.nextFloat();
+        System.out.printf("\nEnter second number :");
+        b = scanner.nextFloat();
 
-        do{
-            System.out.printf("Choose the operation to perform on the random variables:\n");
-            System.out.printf("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Modulus\n6.Exit\n");
-            choice = scanner.nextInt();
+        float temp = a;
+        a = b;
+        b = temp;
 
-            switch (choice){
-                case 1:
-                    System.out.printf("a + b = " + (a+b) +"\n");
-                    break;
+        System.out.printf("\nSwapped numbers :\nfirst number = " + a + "\nsecond number = " + b + "\n");
 
-                case 2:
-                    System.out.printf("a - b = " + (a-b) +"\n");
-                    break;
+        System.out.printf("\n\nSQUARE OF GIVEN NUMBER\nEnter number :");
+        a = scanner.nextFloat();
+        System.out.printf("square = " + a * a);
 
-                case 3:
-                    System.out.printf("a * b = " + (a*b) +"\n");
-                    break;
+        System.out.printf("\n\nCUBE OF GIVEN NUMBER\nEnter number :");
+        a = scanner.nextFloat();
+        System.out.printf("cube = " + a * a * a);
 
-                case 4:
-                    if(b==0){
-                        System.out.printf("Division cannot be performed!\ninvalid numbers.\n");
-                    }else{
-                        System.out.printf("a / b = " + (a/b) +"\n");
-                    }
-                    break;
+        System.out.printf("\n\nCHECK EVEN/ODD\nEnter number :");
+        a = scanner.nextFloat();
+        if (a % 2 == 0) {
+            System.out.printf("\nThe number is even.\n");
+        } else {
+            System.out.printf("\nThe number is odd.\n");
+        }
 
-                case 5://not running!!!
-                    if(b==0){
-                        System.out.printf("Division cannot be performed!\ninvalid numbers.\n");
-                    }else{
-                        System.out.printf("a % b = " + (a%b) +"\n");
-                    }
-                    break;
-            }
-        }while(choice != 6);
+        System.out.printf("\n\nLARGEST OF 3 NUMBERS\nEnter first number :");
+        a = scanner.nextFloat();
+        System.out.printf("Enter second number :");
+        b = scanner.nextFloat();
+        System.out.printf("Enter third number :");
+        c = scanner.nextFloat();
+        float largest;
+
+        if (a > b) {
+            if (a > c) largest = a;
+            else largest = c;
+        } else {
+            if (b > c) largest = b;
+            else largest = c;
+        }
+
+        System.out.printf("Largest among the 3 is " + largest);
+
+        System.out.printf("\n\nCHECK POSITIVE/NEGATIVE/ZERO\nEnter the number :");
+        a = scanner.nextFloat();
+        if (a > 0) {
+            System.out.printf("The number is positive.\n");
+        } else if (a < 0) {
+            System.out.printf("The number is negative.\n");
+        } else {
+            System.out.printf("The number is zero.\n");
+        }
+
+        System.out.printf("\n\nCHECK WHETHER LEAP YEAR\nEnter the year :");
+        a = scanner.nextFloat();
+        if (a % 4 == 0) System.out.printf("\nIt is a leap year!\n");
+        else System.out.printf("\nIt is NOT a leap year.\n");
+
+        System.out.printf("\n\nSUM OF N NUMBERS\nEnter the number of numbers :");
+        a = scanner.nextFloat();
+        System.out.printf("\nSum = " + a * (a + 1) / 2);
+
+        System.out.printf("\n\nREVERSE THE GIVEN NUMBER\nEnter the number :");
+        a = scanner.nextFloat();
+        String n = String.valueOf(a);
+        String rev = "";
+        for (int i = 0; i < n.length(); i++) {
+            rev = i + rev;
+        }
+        System.out.printf("Reversed number = " + rev);
+
+        System.out.printf("\n\nCHECK PALINDROME\nEnter the number :");
+        a = scanner.nextFloat();
+        String first = String.valueOf(a);
+        String r = "";
+        for (int i = 0; i < n.length(); i++) {
+            r = i + r;
+        }
+        if (first == r) System.out.printf("\nIt is palindrome!\n");
+        else System.out.printf("\nIt is not a palindrome!\n");
+
+        System.out.printf("\n\nCHECK PRIME\nEnter the number :");
+        a = scanner.nextFloat();
+
+
+        System.out.printf("\n\nFIND FACTORIAL\nEnter number :");
+        a = scanner.nextFloat();
+        float factorial = 1;
+        for (int i = 2; i <= a; i++) {
+            factorial *= i;
+        }
+        System.out.printf("\nfactorial is " + factorial);
+
+        System.out.printf("\n\nFIBONACCI SERIES\nEnter n :");
+        a = scanner.nextFloat();
+        float prev = 0;
+        float t = 1;
+        System.out.printf(prev + " ");
+        for(int i=0; i<a-2; i++){
+            System.out.printf(t + " ");
+            t = t+prev;
+            prev = t - prev;
+        }
         scanner.close();
 
     }
